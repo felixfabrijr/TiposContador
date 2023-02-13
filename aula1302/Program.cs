52b73e666b66783013798c3aadaec965117ecefe
felixfabri <felix.fabrijr@gmail.com>
2023-02-13 09:21:15 +0000

    Add project files.

---
+++ aula1302/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace aula1302
+{
+    class Program
+    {
+        // Tipos Contador
+        static void Main(string[] args)
+        {
+            int contador = 0;
+            do
+            {
+                Console.WriteLine($" O contador esta em: {contador}");
+                contador++;
+            } while( contador < 10 );
+
+
+            //int contador = 0;
+            //while (contador < 10)
+            //{
+            //    Console.WriteLine($" O contador esta: { contador}");
+            //    contador++;
+            //}
+
+
+            //for (int contador = 0; contador < 10; contador++)
+            //{
+            //    Console.WriteLine($"contador esta {contador}");
+            //}
+
+
+        }
+    }
+}
